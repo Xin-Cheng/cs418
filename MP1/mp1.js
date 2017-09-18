5289e259b06606eba9a8727dfd5ae424041ec241
Xin <x_cheng@outlook.com>
2017-09-17 17:18:49 -0700

    add animation

--- MP1/mp1.js
+++ MP1/mp1.js
@@ -1,4 +1,3 @@
-
 var gl;
 var canvas;
 var shaderProgram;
@@ -128,80 +127,80 @@ function setupShaders() {
 /**
  * Populate buffers with data
  */
+var triangleVertices = [
+  -0.90,0.95,0.00,
+  -0.90,0.64,0.00,
+  0.88,0.95,0.00,
+  -0.90,0.64,0.00,
+  0.88,0.95,0.00,
+  0.88,0.64,0.00,
+  -0.72,0.64,0.00,
+  -0.72,-0.31,0.00,
+  -0.34,0.64,0.00,
+  -0.72,-0.31,0.00,
+  -0.34,0.64,0.00,
+  -0.34,-0.31,0.00,
+  0.32,0.64,0.00,
+  0.32,-0.31,0.00,
+  0.71,0.64,0.00,
+  0.32,-0.31,0.00,
+  0.71,0.64,0.00,
+  0.71,-0.31,0.00,
+  -0.34,0.38,0.00,
+  -0.34,-0.06,0.00,
+  -0.19,0.38,0.00,
+  -0.34,-0.06,0.00,
+  -0.19,0.38,0.00,
+  -0.19,-0.06,0.00,
+  0.17,0.38,0.00,
+  0.17,-0.06,0.00,
+  0.32,0.38,0.00,
+  0.17,-0.06,0.00,
+  0.32,0.38,0.00,
+  0.32,-0.06,0.00,
+  -0.72,-0.38,0.00,
+  -0.72,-0.50,0.00,
+  -0.60,-0.38,0.00,
+  -0.72,-0.50,0.00,
+  -0.60,-0.38,0.00,
+  -0.60,-0.58,0.00,
+  -0.46,-0.38,0.00,
+  -0.46,-0.66,0.00,
+  -0.34,-0.38,0.00,
+  -0.46,-0.66,0.00,
+  -0.34,-0.38,0.00,
+  -0.34,-0.74,0.00,
+  -0.20,-0.38,0.00,
+  -0.20,-0.83,0.00,
+  -0.08,-0.38,0.00,
+  -0.20,-0.83,0.00,
+  -0.08,-0.38,0.00,
+  -0.08,-0.91,0.00,
+  0.06,-0.38,0.00,
+  0.06,-0.91,0.00,
+  0.18,-0.38,0.00,
+  0.06,-0.91,0.00,
+  0.18,-0.38,0.00,
+  0.18,-0.83,0.00,
+  0.32,-0.38,0.00,
+  0.32,-0.74,0.00,
+  0.45,-0.38,0.00,
+  0.32,-0.74,0.00,
+  0.45,-0.38,0.00,
+  0.45,-0.66,0.00,
+  0.58,-0.38,0.00,
+  0.58,-0.58,0.00,
+  0.71,-0.38,0.00,
+  0.58,-0.58,0.00,
+  0.71,-0.38,0.00,
+  0.71,-0.50,0.00  
+];
 function setupBuffers() {
   vertexPositionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
   var number = 6*11;
   // Fill in the position data of badge
-  var triangleVertices = [
-    -0.90,0.95,0.00,
-    -0.90,0.64,0.00,
-    0.88,0.95,0.00,
-    -0.90,0.64,0.00,
-    0.88,0.95,0.00,
-    0.88,0.64,0.00,
-    -0.72,0.64,0.00,
-    -0.72,-0.31,0.00,
-    -0.34,0.64,0.00,
-    -0.72,-0.31,0.00,
-    -0.34,0.64,0.00,
-    -0.34,-0.31,0.00,
-    0.32,0.64,0.00,
-    0.32,-0.31,0.00,
-    0.71,0.64,0.00,
-    0.32,-0.31,0.00,
-    0.71,0.64,0.00,
-    0.71,-0.31,0.00,
-    -0.34,0.38,0.00,
-    -0.34,-0.06,0.00,
-    -0.19,0.38,0.00,
-    -0.34,-0.06,0.00,
-    -0.19,0.38,0.00,
-    -0.19,-0.06,0.00,
-    0.17,0.38,0.00,
-    0.17,-0.06,0.00,
-    0.32,0.38,0.00,
-    0.17,-0.06,0.00,
-    0.32,0.38,0.00,
-    0.32,-0.06,0.00,
-    -0.72,-0.38,0.00,
-    -0.72,-0.50,0.00,
-    -0.60,-0.38,0.00,
-    -0.72,-0.50,0.00,
-    -0.60,-0.38,0.00,
-    -0.60,-0.58,0.00,
-    -0.46,-0.38,0.00,
-    -0.46,-0.66,0.00,
-    -0.34,-0.38,0.00,
-    -0.46,-0.66,0.00,
-    -0.34,-0.38,0.00,
-    -0.34,-0.74,0.00,
-    -0.20,-0.38,0.00,
-    -0.20,-0.83,0.00,
-    -0.08,-0.38,0.00,
-    -0.20,-0.83,0.00,
-    -0.08,-0.38,0.00,
-    -0.08,-0.91,0.00,
-    0.06,-0.38,0.00,
-    0.06,-0.91,0.00,
-    0.18,-0.38,0.00,
-    0.06,-0.91,0.00,
-    0.18,-0.38,0.00,
-    0.18,-0.83,0.00,
-    0.32,-0.38,0.00,
-    0.32,-0.74,0.00,
-    0.45,-0.38,0.00,
-    0.32,-0.74,0.00,
-    0.45,-0.38,0.00,
-    0.45,-0.66,0.00,
-    0.58,-0.38,0.00,
-    0.58,-0.58,0.00,
-    0.71,-0.38,0.00,
-    0.58,-0.58,0.00,
-    0.71,-0.38,0.00,
-    0.71,-0.50,0.00  
-  ];
-
+  
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
   vertexPositionBuffer.itemSize = 3;
   vertexPositionBuffer.numberOfItems = number;
@@ -234,7 +233,7 @@ function draw() {
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   mat4.identity(mvMatrix);
-  mat4.rotateX(mvMatrix, mvMatrix, degToRad(rotAngle));
+  mat4.rotateY(mvMatrix, mvMatrix, degToRad(rotAngle));
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
   gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
     vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
@@ -249,6 +248,7 @@ function draw() {
 /**
  * Animation to be called from tick. Updates globals and performs animation for each tick.
  */
+var sinscalar = 0;
 function animate() {
   var timeNow = new Date().getTime();
   if (lastTime != 0) {
@@ -256,6 +256,16 @@ function animate() {
     rotAngle = (rotAngle + 1.0) % 360;
   }
   lastTime = timeNow;
+  sinscalar += 0.1;
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+  var newVertices = [];
+  for(var i = 0; i < 66 * 3; i+=3) {
+    var point = [triangleVertices[i] , triangleVertices[i+1] + Math.sin(sinscalar+triangleVertices[i])*0.5,  0.0];
+    newVertices = newVertices.concat(point);
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(newVertices), gl.STATIC_DRAW);
+  vertexPositionBuffer.itemSize = 3;
+  vertexPositionBuffer.numberOfItems = 66;
 }
 
 /**
